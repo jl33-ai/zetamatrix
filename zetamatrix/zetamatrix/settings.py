93e0a6e0699bee9594b97347e98e1a2492a001e0
jl33-ai <justinkhlee27@gmail.com>
2024-02-11 10:23:43 +1100

    minus 1 hour

--- zetamatrix/zetamatrix/settings.py
+++ zetamatrix/zetamatrix/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'https://pricey-example-production.up.railway.app/accounts/signup/']
 
 DATABASES = {
     "default": {
