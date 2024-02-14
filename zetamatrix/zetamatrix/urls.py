7a3e070450164ebc02296ec171cbc124def3aebc
jl33-ai <justinkhlee27@gmail.com>
2024-02-14 19:34:22 +1100

    trying static files

--- zetamatrix/zetamatrix/urls.py
+++ zetamatrix/zetamatrix/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from game import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('game/', include('game.urls')),
     path('', views.home, name='home')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
