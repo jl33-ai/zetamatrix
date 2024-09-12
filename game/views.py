a314cb6b983824db0a128fc39a3d4e0a8fc023f9
jl33-ai <justinkhlee27@gmail.com>
2024-09-12 17:53:55 +1000

    UI changes WIP

--- game/views.py
+++ game/views.py
@@ -12,37 +12,41 @@ from django.db.models import Max, Avg
 import numpy as np
 from datetime import date
 
+MAGIC_NUMBER = 3182
+
 
 # Create your views here.
 def home(request):
     # fetch leaderboard
-    seven_days_ago = timezone.now()-timedelta(days=7)
-    top_scores = GameSession.objects.filter(start_time__gte=seven_days_ago, is_dailychallenge=False)\
-                                     .values('user__username')\
-                                     .annotate(max_score=Max('score'))\
-                                     .order_by('-max_score')[:10]  # Adjust the number as needed
-    
-    daily_challenge_scores = GameSession.objects.filter(start_time__gte=date.today(), is_dailychallenge=True)\
-                                     .values('user__username')\
-                                     .annotate(max_score=Max('score'))\
-                                     .order_by('-max_score')[:5]  
-    
+    seven_days_ago = timezone.now() - timedelta(days=365)
+    top_scores = GameSession.objects.filter(start_time__gte=seven_days_ago, is_dailychallenge=False) \
+                     .values('user__username') \
+                     .annotate(max_score=Max('score')) \
+                     .order_by('-max_score')[:10]  # Adjust the number as needed
+
+    daily_challenge_scores = GameSession.objects.filter(start_time__gte=date.today(), is_dailychallenge=True) \
+                                 .values('user__username') \
+                                 .annotate(max_score=Max('score')) \
+                                 .order_by('-max_score')[:5]
+
     num_total_users = User.objects.count()
 
     context = {
         'top_scores': top_scores,
         'dc_scores': daily_challenge_scores,
-        'username' : request.user.username,
-        'num_total_users' : 42+int(num_total_users)
-        }
+        'username': request.user.username,
+        'num_total_users': MAGIC_NUMBER + int(num_total_users)
+    }
 
-    if request.user.is_authenticated: 
-        context['stats']="Your summary statistics here"
+    if request.user.is_authenticated:
+        context['stats'] = "Your summary statistics here"
     return render(request, 'home.html', context)
+
 
 def seematrix(request):
     context = {}
     return render(request, 'game/zetamatrix.html', context)
+
 
 def about_page(request):
     return render(request, 'about.html')
@@ -57,7 +61,8 @@ def start_game(request):
     else:
         # Redirect to home if the request is not POST
         return redirect('home')
-        
+
+
 @login_required
 def start_dailychallenge(request):
     # Can only play IF: they've answered 100 questions (pull from PlayerStats)
@@ -66,36 +71,38 @@ def start_dailychallenge(request):
     # and then pass it to the page with 'playing_dailychallenge = True'
     if request.method != 'POST':
         return render(request, 'loadfail.html', context={"reason": "Did not request page from home page."})
-    
+
     player = request.user
 
     player_stats, created = PlayerStats.objects.get_or_create(
-            user=player,
-            defaults={
-                'games_played': 0,
-                'questions_answered': 0,
-                'has_world_record': False,
-                'has_come_top_50_daily_challenge': False
-            }
-        )
-    if created or player_stats.questions_answered < 10: 
-        return render(request, 'loadfail.html', context={"reason": "You need to answer at least 10 questions in normal mode first."})
-    
+        user=player,
+        defaults={
+            'games_played': 0,
+            'questions_answered': 0,
+            'has_world_record': False,
+            'has_come_top_50_daily_challenge': False
+        }
+    )
+    if created or player_stats.questions_answered < 10:
+        return render(request, 'loadfail.html',
+                      context={"reason": "You need to answer at least 10 questions in normal mode first."})
+
     today = date.today()
     if GameSession.objects.filter(user=player, start_time__date=today, is_dailychallenge=True).exists():
-        return render(request, 'loadfail.html', context={"reason": "You have already attempted today's daily challenge."})
+        return render(request, 'loadfail.html',
+                      context={"reason": "You have already attempted today's daily challenge."})
 
-    try: 
+    try:
         daily_challenge = DailyChallenge.objects.get(date=today)
     except DailyChallenge.DoesNotExist:
         return render(request, 'loadfail.html', context={"reason": "The daily zetamac hasn't been written yet."})
-    
+
     questions = daily_challenge.questions
     return render(request, 'zetamac_daily.html', {'game_length': 150, 'questions': questions})
 
-    
+
 @csrf_exempt  # For simplicity, might need proper CSRF handling for production
-#@require_POST  # Ensures that only POST requests are accepted
+# @require_POST  # Ensures that only POST requests are accepted
 @login_required  # Ensures the user is authenticated
 def save_game_data(request):
     """
@@ -153,8 +160,8 @@ def save_game_data(request):
         if not player_stats.has_world_record:
             # Now check if they are the top of the 7-day leaderboard
             seven_days_ago = timezone.now() - timedelta(days=7)
-            top_score = GameSession.objects.filter(start_time__gte=seven_days_ago)\
-                                        .aggregate(max_score=Max('score'))['max_score']
+            top_score = GameSession.objects.filter(start_time__gte=seven_days_ago) \
+                .aggregate(max_score=Max('score'))['max_score']
 
             if top_score is not None and data['score'] >= top_score:
                 player_stats.has_world_record = True
@@ -166,7 +173,6 @@ def save_game_data(request):
         return JsonResponse({'status': 'error', 'message': f'Missing key: {e}'}, status=400)
     except Exception as e:  # Consider more specific exception handling
         return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
-    
 
 
 # ZETAMATRICES
@@ -177,19 +183,20 @@ def see_addition(request):
         .values('num1', 'num2') \
         .annotate(average_time=Avg('time_taken')) \
         .order_by('num1', 'num2')
-    
-    heatmap_data = [[0]*101 for _ in range(101)]
-    i=2
+
+    heatmap_data = [[0] * 101 for _ in range(101)]
+    i = 2
     for row in data:
-        if row['num1']==i:
-            heatmap_data[i][row['num2']]=np.log(min(10000, row['average_time']))
+        if row['num1'] == i:
+            heatmap_data[i][row['num2']] = np.log(min(10000, row['average_time']))
         else:
-            i+=1
+            i += 1
 
     return render(request, 'matrices/addition_zetamatrix.html', {
-        'heatmap_data': json.dumps(heatmap_data), 
+        'heatmap_data': json.dumps(heatmap_data),
         'num_contributions': SolvedAddition.objects.count(),
-        })
+    })
+
 
 def see_subtraction(request):
     # As DJango ORM query
@@ -197,23 +204,25 @@ def see_subtraction(request):
         .values('num1', 'num2') \
         .annotate(average_time=Avg('time_taken')) \
         .order_by('num1', 'num2')
-    
-    heatmap_data = [[0]*101 for _ in range(101)]
-    i=2
+
+    heatmap_data = [[0] * 101 for _ in range(101)]
+    i = 2
     for row in data:
-        if row['num1']==i:
-            heatmap_data[i][row['num2']]=np.log(min(10000, row['average_time']))
+        if row['num1'] == i:
+            heatmap_data[i][row['num2']] = np.log(min(10000, row['average_time']))
         else:
-            i+=1
+            i += 1
 
     return render(request, 'matrices/addition_zetamatrix.html', {
-        'heatmap_data': json.dumps(heatmap_data), 
+        'heatmap_data': json.dumps(heatmap_data),
         'num_contributions': SolvedSubtraction.objects.count(),
-        })
+    })
+
 
 def see_division(request):
     # As DJango ORM query
     return render(request, 'home.html')
+
 
 def see_multiplication(request):
     # As DJango ORM query
@@ -221,17 +230,16 @@ def see_multiplication(request):
         .values('num1', 'num2') \
         .annotate(average_time=Avg('time_taken')) \
         .order_by('num1', 'num2')
-    
-    heatmap_data = [[0]*101 for _ in range(11+2)]
-    i=2
+
+    heatmap_data = [[0] * 101 for _ in range(11 + 2)]
+    i = 2
     for row in data:
-        if row['num1']==i:
-            heatmap_data[i][row['num2']]=np.log(min(10000, row['average_time']))
+        if row['num1'] == i:
+            heatmap_data[i][row['num2']] = np.log(min(10000, row['average_time']))
         else:
-            i+=1
+            i += 1
 
     return render(request, 'matrices/multiplication_zetamatrix.html', {
         'heatmap_data': json.dumps(heatmap_data),
         'num_contributions': SolvedMultiplication.objects.count(),
-        })
-
+    })
